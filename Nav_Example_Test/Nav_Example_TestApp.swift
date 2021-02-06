a0779925056393b0dcf2060b16c7dcb6eb69c617
davidakoontz <david@koontz.name>
2021-02-06 17:39:01 -0600

    Initial Commit

---
+++ Nav_Example_Test/Nav_Example_TestApp.swift
@@ -0,0 +1,17 @@
+//
+//  Nav_Example_TestApp.swift
+//  Nav_Example_Test
+//
+//  Created by David on 2/6/21.
+//
+
+import SwiftUI
+
+@main
+struct Nav_Example_TestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Nav_Example_TestTests/Nav_Example_TestTests.swift
@@ -0,0 +1,33 @@
+//
+//  Nav_Example_TestTests.swift
+//  Nav_Example_TestTests
+//
+//  Created by David on 2/6/21.
+//
+
+import XCTest
+@testable import Nav_Example_Test
+
+class Nav_Example_TestTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
